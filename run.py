9653343a48979b4bb805fa5f8c9af3e51a600c96
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-02-25 16:25:42 -0500

    no message

--- run.py
+++ run.py
@@ -11,12 +11,7 @@ app.config['PROPAGATE_EXCEPTIONS']=True
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
 def index():
-    
-    return render_template("index.html" )
-    
-@app.route("/zoomable.html", methods=["GET", "POST"])
-def zoomable():
-    if request.method=='POST':
+	if request.method=='POST':
     	title=request.form['title']
     	subA=request.form['subjectA']
     	subB=request.form['subjectB']
@@ -24,6 +19,19 @@ def zoomable():
     	neutralColor=request.form['nColor']
     	colorA=request.form['aColor']
     	colorB=request.form['bColor']
+    
+    return render_template("index.html" )
+    
+@app.route("/zoomable.html", methods=["GET", "POST"])
+def zoomable():
+    # if request.method=='POST':
+#     	title=request.form['title']
+#     	subA=request.form['subjectA']
+#     	subB=request.form['subjectB']
+#     	lvZero=request.form['pfColor']
+#     	neutralColor=request.form['nColor']
+#     	colorA=request.form['aColor']
+#     	colorB=request.form['bColor']
     
     return render_template("zoomable.html" )
     
