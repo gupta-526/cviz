208ed5864e6caf8c98921c344d41ca700f936938
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-02-25 16:36:24 -0500

    no message

--- run.py
+++ run.py
@@ -11,27 +11,20 @@ app.config['PROPAGATE_EXCEPTIONS']=True
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
 def index():
-    if request.method=='POST':
-        title=request.form['title']
-        subA=request.form['subjectA']
-        subB=request.form['subjectB']
-        lvZero=request.form['pfColor']
-        neutralColor=request.form['nColor']
-		colorA=request.form['aColor']
-		colorB=request.form['bColor']
+    
     
     return render_template("index.html" )
     
 @app.route("/zoomable.html", methods=["GET", "POST"])
 def zoomable():
-    # if request.method=='POST':
-#     	title=request.form['title']
-#     	subA=request.form['subjectA']
-#     	subB=request.form['subjectB']
-#     	lvZero=request.form['pfColor']
-#     	neutralColor=request.form['nColor']
-#     	colorA=request.form['aColor']
-#     	colorB=request.form['bColor']
+	if request.method=='POST':
+    	title=request.form['title']
+    	subA=request.form['subjectA']
+    	subB=request.form['subjectB']
+    	lvZero=request.form['pfColor']
+  		neutralColor=request.form['nColor']
+		colorA=request.form['aColor']
+		colorB=request.form['bColor']
     
     return render_template("zoomable.html" )
     
