dd5628d8c9fdcaef4aff7014bd7a4f967f29c5ec
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-02-25 16:30:59 -0500

    no message

--- run.py
+++ run.py
@@ -11,14 +11,14 @@ app.config['PROPAGATE_EXCEPTIONS']=True
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
 def index():
-	if request.method=='POST':
-    	title=request.form['title']
-    	subA=request.form['subjectA']
-    	subB=request.form['subjectB']
-    	lvZero=request.form['pfColor']
-    	neutralColor=request.form['nColor']
-    	colorA=request.form['aColor']
-    	colorB=request.form['bColor']
+    if request.method=='POST':
+        title=request.form['title']
+        subA=request.form['subjectA']
+        subB=request.form['subjectB']
+        lvZero=request.form['pfColor']
+        neutralColor=request.form['nColor']
+		colorA=request.form['aColor']
+		colorB=request.form['bColor']
     
     return render_template("index.html" )
     
