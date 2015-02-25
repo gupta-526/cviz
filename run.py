b71537e11a4df548d37a96846adc06de008d5009
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-02-25 16:42:01 -0500

    no message

--- run.py
+++ run.py
@@ -11,29 +11,29 @@ app.config['PROPAGATE_EXCEPTIONS']=True
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
 def index():
-    
-    
-    return render_template("index.html" )
-    
+	
+	
+	return render_template("index.html" )
+	
 @app.route("/zoomable.html", methods=["GET", "POST"])
 def zoomable():
 	if request.method=='POST':
 	
-    	title=request.form['title']
-    	subA=request.form['subjectA']
-    	subB=request.form['subjectB']
-    	lvZero=request.form['pfColor']
-  		neutralColor=request.form['nColor']
-		colorA=request.form['aColor']
-		colorB=request.form['bColor']
-    
-    return render_template("zoomable.html" )
-    
+		title=request.form['title']
+			subA=request.form['subjectA']
+			subB=request.form['subjectB']
+			lvZero=request.form['pfColor']
+		    neutralColor=request.form['nColor']
+		    colorA=request.form['aColor']
+		    colorB=request.form['bColor']
+	
+	return render_template("zoomable.html" )
+	
 # def insult():
-#     return app.root_path
+#	  return app.root_path
 #@app.route("/hello/<name>")
 #def input(name=None):
-    #return render_template('inputData.html')
+	#return render_template('inputData.html')
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
