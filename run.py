40df2d931bc56211b8fb19f8d9614d86968d16c4
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-03-04 15:52:31 -0500

    trying with uploading files documentation present in flask

--- run.py
+++ run.py
@@ -2,9 +2,28 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
+from werkzeug import SharedDataMiddleware
+from werkzeug import secure_filename
+
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+
+UPLOAD_FOLDER="/app"
+
+ALLOWED_EXTENSIONS='json'
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 #Keeps Flask from swallowing error messages
 app.config['PROPAGATE_EXCEPTIONS']=True
 
@@ -17,10 +36,16 @@ def index():
 	
 @app.route("/zoomable.html", methods=["GET", "POST"])
 def zoomable():
-  	    
+  	    file = request.files['myFile']
+  	    if file and allowed_file(file.filename):
+            filename=secure_filename(file.filename)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
                             subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
-                            colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=request.files['myFiles'] )
+                            colorA=request.form['aColor'], colorB=request.form['bColor'], 
+                            reqFile=redirect(url_for('uploaded_file',
+                                    filename=filename)))
 	
 if __name__ == '__main__':
 	app.run()
