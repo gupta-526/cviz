94c22a2f5758dbbcc1620882b612960d6071eac3
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-03-06 16:46:02 -0500

    no message

--- run.py
+++ run.py
@@ -4,28 +4,34 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import send_from_directory
 from werkzeug import SharedDataMiddleware
 from werkzeug import secure_filename
-
+ 
+ #get the upload function in a separate function so that zoomable isnt doing so many things
+ #a different form for the upload files?? if so how to link the rest of the data you get from index
+ #pass file name/url/path to the d3.js location required. 
 
 UPLOAD_FOLDER="/"
 
 ALLOWED_EXTENSIONS='json'
 
 app = Flask(__name__)
-
+#Keeps Flask from swallowing error messages
+app.config['PROPAGATE_EXCEPTIONS']=True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.add_url_rule('/uploads/myFiles', 'zoomable',
-                 build_only=True)
+
+# app.add_url_rule('/uploads/myFiles', 'zoomable',
+#                  build_only=True)
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-    '/uploads':  app.config['UPLOAD_FOLDER']
+    '/':  app.config['UPLOAD_FOLDER']
 })
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-#Keeps Flask from swallowing error messages
-app.config['PROPAGATE_EXCEPTIONS']=True
+
+
 
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
@@ -33,19 +39,39 @@ def index():
 
 	
     return render_template("index.html" )
+# def uploaded_File():
+#     file = request.files['myFiles']
+#     if file and allowed_file(file.filename):
+#         filename=secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#     return url_for('uploaded_file', filename=filename)
+# 
+# 
+# def getModelType():
+#     model=request.form['simple']
+#     if(model=='simple')
+#         return redirect('/simple')
+#     else 
+#         return redirect('/zoomable')
+    
+        
+
 	
-@app.route("/zoomable.html", methods=["GET", "POST"])
+# def uploaded_File():
+#     file = request.files['myFiles']
+#     if file and allowed_file(file.filename):
+#         filename=secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         return url_for('uploaded_file', filename=filename)
+#         
+    
+@app.route("/zoomable", methods=["GET", "POST"])
 def zoomable():
 
-    file = request.files['myFiles']
-    if file and allowed_file(file.filename):
-        filename=secure_filename(file.filename)
-    
+   
     return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
                             subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
-                            colorA=request.form['aColor'], colorB=request.form['bColor'], 
-                            reqFile=url_for('zoomable',
-                                    filename=filename))
+                            colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=url_for(request.files['myFiles'])
 	
 if __name__ == '__main__':
 	app.run()
