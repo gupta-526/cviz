3781a1c4ad12127a38d9507eb5db48081f891403
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-05 12:51:06 -0400

    no message

--- run.py
+++ run.py
@@ -27,58 +27,58 @@ with app.app_context():
 
     app.config['ALLOWED_EXTENSIONS']='json'
 
-    app.add_url_rule('/uploads/myFiles', 'zoomable',
-                      build_only=True)
-    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-        '/':  app.config['UPLOAD_FOLDER']
-    })
+app.add_url_rule('/uploads/myFiles', 'zoomable',
+                  build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/':  app.config['UPLOAD_FOLDER']
+})
 
-    def allowed_file(filename):
-        return '.' in filename and \
-               filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
 
-    @app.route("/")
-    @app.route("/index", methods=["GET", "POST"])
-    def index():
+@app.route("/")
+@app.route("/index", methods=["GET", "POST"])
+def index():
 
-    
-        return render_template("index.html" )
 
-    # def getModelType():
-    #     model=request.form['simple']
-    #     if(model=='simple')
-    #         return redirect('/simple')
-    #     else 
-    #         return redirect('/zoomable')
+    return render_template("index.html" )
 
-    @app.route('/upload', methods=['POST'])
-    def upload():
-        file = request.files['myFiles']
-        if file and allowed_file(file.filename):
-            filename=secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # url2= url_for('uploaded_file', filename=filename) 
-    #         print "url for upload folder= "%url2;
-        return url_for('uploaded_file', filename=filename) 
+# def getModelType():
+#     model=request.form['simple']
+#     if(model=='simple')
+#         return redirect('/simple')
+#     else 
+#         return redirect('/zoomable')
 
-    @app.route('/uploads/<filename>')
-    def uploaded_file(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'],filename)    
-    
-    @app.route("/zoomable", methods=["GET", "POST"])
-    def zoomable():
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['myFiles']
+    if file and allowed_file(file.filename):
+        filename=secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # url2= url_for('uploaded_file', filename=filename) 
+#         print "url for upload folder= "%url2;
+    return url_for('uploaded_file', filename=filename) 
 
-   
-        return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
-                                subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
-                                colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)    
 
-    @app.route("/simple", methods=["GET","POST"])
-    def simple():
-        return render_template("simple.html", title=request.form['title'], subA=request.form['subjectA'], 
-                                subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
-                                colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
-    if __name__ == '__main__':
-        app.run()
+@app.route("/zoomable", methods=["GET", "POST"])
+def zoomable():
+
+
+    return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
+                            subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
+                            colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
+
+@app.route("/simple", methods=["GET","POST"])
+def simple():
+    return render_template("simple.html", title=request.form['title'], subA=request.form['subjectA'], 
+                            subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
+                            colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
+if __name__ == '__main__':
+    app.run()    
