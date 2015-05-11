d5243566ddb67f1e89e57c52390c363dc50bfb26
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-11 11:06:52 -0400

    no message

--- run.py
+++ run.py
@@ -22,74 +22,74 @@ with app.app_context():
     #print current_app.name
     
     app.config['SERVER_NAME']='circlePacking.local:8080'
-app.config['PROPAGATE_EXCEPTIONS']=True
-app.config['UPLOAD_FOLDER'] = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'),'uploads/')
-#app.config['UPLOAD_FOLDER'] ='Users/purnimakumar/Documents/VisualModelApp/uploads/'
-app.config['ALLOWED_EXTENSIONS']='json'
-#files=UploadSet('files',FILE)   
-#url=url_for(['UPLOAD_FOLDER']);
-#print "url for upload folder= "%url;
+    app.config['PROPAGATE_EXCEPTIONS']=True
+    app.config['UPLOAD_FOLDER'] = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'),'uploads/')
+    #app.config['UPLOAD_FOLDER'] ='Users/purnimakumar/Documents/VisualModelApp/uploads/'
+    app.config['ALLOWED_EXTENSIONS']='json'
+    #files=UploadSet('files',FILE)   
+    #url=url_for(['UPLOAD_FOLDER']);
+    #print "url for upload folder= "%url;
 
-app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-    '/':  app.config['UPLOAD_FOLDER']
-})
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+        '/':  app.config['UPLOAD_FOLDER']
+    })
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-
-
-@app.route("/")
-@app.route("/index", methods=["GET", "POST"])
-def index():
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
-    return render_template("index.html" )
 
-# def getModelType():
-    imageType=request.form['imageType']
-#     print type(imageType)
-    # if(imageType=='simple'):
-#         app.add_url_rule('/uploads/myFiles', 'simple',
-#                   build_only=True) 
-#         return redirect('/simple')
-#     else:
-#         app.add_url_rule('/uploads/myFiles', 'zoomable',
-#                   build_only=True) 
-#         return redirect('/zoomable')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['myFiles']
-    if file and allowed_file(file.filename):
-        filename=secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return url_for('uploaded_file', filename=filename) 
-
-    # if 'myFiles' in request.files:
-#         filename=files.save(request.files['myFiles'])
-#         rec=File(filename=filename, user=g.user.id)
-#         rec.stire()
-#     re
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)    
-
-@app.route("/zoomable", methods=["GET", "POST"])
-def zoomable():
+    @app.route("/")
+    @app.route("/index", methods=["GET", "POST"])
+    def index():
 
 
-    return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
-                            subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
-                            colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
+        return render_template("index.html" )
 
-@app.route("/simple", methods=["GET","POST"])
-def simple():
-    return render_template("simple.html", title=request.form['title'], subA=request.form['subjectA'], 
-                            subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
-                            colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
-if __name__ == '__main__':
-    app.debug=true
-    #app.run(host='0.0.0.0', port=int("5000"))
-    app.run()
+    # def getModelType():
+        imageType=request.form['imageType']
+    #     print type(imageType)
+        # if(imageType=='simple'):
+    #         app.add_url_rule('/uploads/myFiles', 'simple',
+    #                   build_only=True) 
+    #         return redirect('/simple')
+    #     else:
+    #         app.add_url_rule('/uploads/myFiles', 'zoomable',
+    #                   build_only=True) 
+    #         return redirect('/zoomable')
+
+    @app.route('/upload', methods=['POST'])
+    def upload():
+        file = request.files['myFiles']
+        if file and allowed_file(file.filename):
+            filename=secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return url_for('uploaded_file', filename=filename) 
+
+        # if 'myFiles' in request.files:
+    #         filename=files.save(request.files['myFiles'])
+    #         rec=File(filename=filename, user=g.user.id)
+    #         rec.stire()
+    #     re
+    @app.route('/uploads/<filename>')
+    def uploaded_file(filename):
+        return send_from_directory(app.config['UPLOAD_FOLDER'],filename)    
+
+    @app.route("/zoomable", methods=["GET", "POST"])
+    def zoomable():
+
+
+        return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
+                                subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
+                                colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
+
+    @app.route("/simple", methods=["GET","POST"])
+    def simple():
+        return render_template("simple.html", title=request.form['title'], subA=request.form['subjectA'], 
+                                subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
+                                colorA=request.form['aColor'], colorB=request.form['bColor'], reqFile=upload())
+    if __name__ == '__main__':
+        app.debug=true
+        #app.run(host='0.0.0.0', port=int("5000"))
+        app.run()
