384b0431cae4a93e07f7650c0d2acc83c24446f7
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-12 11:50:48 -0400

    no message

--- run.py
+++ run.py
@@ -66,7 +66,7 @@ with app.app_context():
         if file and allowed_file(file.filename):
             filename=secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('upload_fille',filename=filename)) 
+        return redirect(url_for('uploaded_file',filename=filename)) 
    
     @app.route('/uploads/<filename>')
     def uploaded_file(filename):
