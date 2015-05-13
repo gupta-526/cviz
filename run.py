784639470bbf0804354fc0623858ad0c40d9538b
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-13 09:42:30 -0400

    no message

--- run.py
+++ run.py
@@ -80,7 +80,7 @@ with app.app_context():
         return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
                                 subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
                                 colorA=request.form['aColor'], colorB=request.form['bColor'], 
-                                reqFile=send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+                                reqFile=os.path.join(aap.config['UPLOAD_FOLDER'],filename)
 
     # @app.route('/simple', methods=["GET","POST"])
 #     def simple(filename):
@@ -89,7 +89,7 @@ with app.app_context():
 #                                 subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
 #                                 colorA=request.form['aColor'], colorB=request.form['bColor'], 
 #                                 reqFile=os.path.join(aap.config['UPLOAD_FOLDER'],filename))
+
 if __name__ == '__main__':
-    app.debug=true
     #app.run(host='0.0.0.0', port=int("5000"))
     app.run()
