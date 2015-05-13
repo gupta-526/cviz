faaa2857194abdeb0bfd7850048ec0d55505a7ae
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-13 09:34:44 -0400

    truing send_from_directory instead or os.path.join

--- run.py
+++ run.py
@@ -75,20 +75,20 @@ with app.app_context():
 
     @app.route('/zoomable/<filename>', methods=["GET", "POST"])
     def zoomable(filename):
-
+        flash('goes in the zoomable(filename) def')
 
         return render_template("zoomable.html", title=request.form['title'], subA=request.form['subjectA'], 
                                 subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
                                 colorA=request.form['aColor'], colorB=request.form['bColor'], 
-                                reqFile=os.path.join(app.config['UPLOAD_FOLDER'],filename))
+                                reqFile=send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
-    @app.route("/simple", methods=["GET","POST"])
+    @app.route("/simple/<filename>", methods=["GET","POST"])
     def simple(filename):
     
         return render_template("simple.html", title=request.form['title'], subA=request.form['subjectA'], 
                                 subB=request.form['subjectB'], neutralColor=request.form['nColor'], 
                                 colorA=request.form['aColor'], colorB=request.form['bColor'], 
-                                reqFile=os.path.join(aap.config['UPLOAD_FOLDER'],upload()))
+                                reqFile=os.path.join(aap.config['UPLOAD_FOLDER'],filename))
 if __name__ == '__main__':
     app.debug=true
     #app.run(host='0.0.0.0', port=int("5000"))
