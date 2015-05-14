61da6bf6dafa2f81d201f8807a2b6b126dbdbe09
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-14 15:06:12 -0400

    no message

--- run.py
+++ run.py
@@ -72,14 +72,14 @@ with app.app_context():
 
     
       @app.route('/upload', methods=['POST'])
-        def upload():
-            file = request.files['myFiles']
-            filename=""
-            if file and allowed_file(file.filename):
-                filename=secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    #             path=url_for(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return  filename
+      def upload():
+        file = request.files['myFiles']
+        filename=""
+        if file and allowed_file(file.filename):
+            filename=secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             path=url_for(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return  filename
         
         
 
