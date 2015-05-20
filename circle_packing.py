2b911cada51af1ac2f9b0a939beadf9e7f0df70b
Niharika Gupta <purnimakumar@purnimaarsimac3.dentnet.dent.ohio-state.edu>
2015-05-20 12:57:13 -0400

    no message

--- circle_packing.py
+++ circle_packing.py
@@ -1,8 +1,11 @@
+import csv
+import json
+
 def mtable_to_json(mg_abundance):
-#     """
-#     Convert a list of functional abundance data (level1,level2,level3,level4,abundance) into 
-#     a hierarchical JSON file: {name: ..., children: [name:..., size:...]}
-#     """
+    """
+    Convert a list of functional abundance data (level1,level2,level3,level4,abundance) into
+    a hierarchical JSON file: {name: ..., children: [name:..., size:...]}
+    """
     hierarchy = {'name': 'metagenome', 'children':[]}
     for entry in mg_abundance:
         L1_idx = -1
@@ -12,9 +15,9 @@ def mtable_to_json(mg_abundance):
                 L1_idx = i
                 break
         else:
-            hierarchy['children'].append({'name':lvl1, 
-                                          'children':[{'name':lvl2, 
-                                                       'children':[{'name':lvl3, 
+            hierarchy['children'].append({'name':lvl1,
+                                          'children':[{'name':lvl2,
+                                                       'children':[{'name':lvl3,
                                                                     'children':[{'name':lvl4, 'size':count}]}]}]})
             continue
         if L1_idx > -1:
@@ -24,8 +27,8 @@ def mtable_to_json(mg_abundance):
                     L2_idx = j
                     break
             else:
-                hierarchy['children'][L1_idx]['children'].append({'name': lvl2, 
-                                                                  'children':[{'name':lvl3, 
+                hierarchy['children'][L1_idx]['children'].append({'name': lvl2,
+                                                                  'children':[{'name':lvl3,
                                                                                'children':[{'name':lvl4, 'size':count}]}]})
                 continue
         if L2_idx > -1:
@@ -34,19 +37,84 @@ def mtable_to_json(mg_abundance):
                     c['children'].append({'name':lvl4, 'size':count})
                     break
             else:
-                hierarchy['children'][L1_idx]['children'][L2_idx]['children'].append({'name': lvl3, 
+                hierarchy['children'][L1_idx]['children'][L2_idx]['children'].append({'name': lvl3,
                                                                                       'children':[{'name':lvl4, 'size':count}]})
-        
+
     return hierarchy
 
 
+def mtable_to_json_3lvls(mg_abundance):
+    """
+    Convert a list of KEGG orthology abundance data (level1,level2,level3,abundance) into
+    a hierarchical JSON file: {name: ..., children: [name:..., size:...]}
+    """
+    hierarchy = {'name': 'metagenome', 'children':[]}
+    for entry in mg_abundance:
+        index = -1
+        lvl1, lvl2, lvl3, count = entry[0], entry[1], entry[2], sum(map(float,entry[3:]))
+        for i,c in enumerate(hierarchy['children']):
+            if c['name'] == lvl1:
+                index = i
+                break
+        else:
+            hierarchy['children'].append({'name':lvl1, 'children':[{'name':lvl2, 'children':[{'name':lvl3, 'size':count}]}]})
+            continue
+        if index > -1:
+            for c in hierarchy['children'][index]['children']:
+                if c['name'] == lvl2:
+                    c['children'].append({'name':lvl3, 'size':count})
+                    break
+            else:
+                hierarchy['children'][index]['children'].append({'name': lvl2, 'children':[{'name':lvl3, 'size':count}]})
+    return hierarchy
 
-def process_fc_data(fc_lvl_fp, json_out_fp, delim='\t'):
-#     """
-#     Takes a tab-delimited spreadsheet file as input with the f
-#     """
+
+def mtable_to_json_2lvls(mg_abundance):
+    """
+    Convert a list of KEGG orthology abundance data (level1,level2,abundance) into
+    a hierarchical JSON file: {name: ..., children: [name:..., size:...]}
+    """
+    hierarchy = {'name': 'metagenome', 'children':[]}
+    for entry in mg_abundance:
+        index = -1
+        lvl1, lvl2, count = entry[0], entry[1], sum(map(float,entry[2:]))
+        for i,c in enumerate(hierarchy['children']):
+            if c['name'] == lvl1:
+                index = i
+                break
+        else:
+            hierarchy['children'].append({'name':lvl1, 'children':[{'name':lvl2, 'size':count}]})
+            continue
+        if index > -1:
+            hierarchy['children'][index]['children'].append({'name':lvl2, 'size':count})
+    return hierarchy
+
+
+def process_fc_data(fc_lvl_fp, json_out_fp, delim='\t', levels=4):
+    """
+    Takes as input a spreadsheet file containing hierarchical pathway/functional
+    data and associated fold-changes (pos/neg indicating group A vs group B) and
+    writes out a JSON file that can be input into the circle-packing viusalization.
+
+    :type fc_lvl_fp: str
+    :param fc_lvl_fp: Path to the fold-change data file.
+    :type json_out_fp: str
+    :param json_out_fp: Path to the output JSON file.
+    :type delim: str
+    :param delim: Optional delimiter for use while reading the input file.
+                  Defaults to the tab character ('\t')
+    :rtype: None
+    """
+    if levels not in range(2,5):
+        raise ValueError("{} invalid value for 'levels' parameter. Valid values: 2, 3, 4")
+    lvl_switch = {4: mtable_to_json, 3: mtable_to_json_3lvls, 2: mtable_to_json_2lvls}
+
     with open(fc_lvl_fp, 'rU') as in_f:
         fc_lvl_data = [line for line in csv.reader(in_f, delimiter=delim)][1:]
 
     with open(json_out_fp, 'w') as out_f:
-        json.dump(mtable_to_json(fc_lvl_data), out_f)
+        json.dump(lvl_switch[levels](fc_lvl_data), out_f)
+
+
+
+
