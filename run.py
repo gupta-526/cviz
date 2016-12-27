8047b82b8b4928e442a4d5a47edde06fd59e4897
gupta-526 <gupta.526@buckeyemail.osu.edu>
2016-12-27 15:17:41 -0500

    filename for the uploaded file
    unchanged filename was being passed. Chainging it in an effort to pass the right random filename

--- run.py
+++ run.py
@@ -96,7 +96,8 @@ with app.app_context():
 	@app.route('/bubble_viz', methods=['GET','POST'])	 
 	def bubble_viz():
 		filename=random_sufix()+'.txt'
-		upload_fp = os.path.join(app.config['UPLOAD_FOLDER'], upload())
+		upload_fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+		upload_fp = upload()
 		param_list = {'title': request.form['title'],
 					   'fontType': request.form['fontList'],
 					   'reqFile': upload_fp}
